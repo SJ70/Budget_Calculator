fbd1f721c489b3f69a9070ee4aab66e687f46134
SJ70 <seojae70@gmail.com>
2024-03-05 12:21:13 +0900

    feat: title 컴포넌트 추가
    - max width 지정
    - 양옆 margin 지정

---
+++ src/components/Title.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Title.css';
+
+const Title: React.FC = () => {
+    return (
+        <div className='title-container'>
+            <h1>
+                예산 계산기
+            </h1>
+        </div>
+    )
+}
+
+export default Title;
